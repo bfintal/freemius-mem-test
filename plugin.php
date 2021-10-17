53e2a24461447fcf3c1a5282848d48b00cbb5bf9
Benjamin Intal <bfintal@gmail.com>
2021-10-17 22:09:01 +0800

    initial commit

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Freemius memory test
+ * Plugin URI: https://wpstackable.com
+ * Description: Freemius memory test
+ * Author: Gambit Technologies, Inc
+ * Author URI: http://gambit.ph
+ * Text Domain: Freemius memory test
+ * Version: 0.1
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// require_once( plugin_dir_path( __FILE__ ) . 'freemius.php' );
